c591805f86f50d23e38c23a02e8a464e05017c88
Alexander Alemayhu <alexander@alemayhu.com>
2021-02-16 22:04:14 +0100

    setup the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ChannelRow from './components/ChannelRow'
+
+let data = [
+  {
+    name: 'Alexander Alemayhu',
+    thumbnail: '/avatar/aa.jpeg',
+    description: 'A personal channel where I share productivity stuff related to ⭐️ Anki and 🄽 Notion.',
+    link: 'https://youtube.com/c/alexanderalemayhu'
+  },
+  {
+    name: 'Alemayhu VODs',
+    thumbnail: '/avatar/portrait_1.jpeg',
+    description: 'Hour long exports from my Twitch channel 😊',
+    link: 'https://www.youtube.com/c/AlemayhuVODS'
+  }
+];
+
 function App() {
+  const channels = data.map(x => {
+    return <li class="self-center justify-center w-4/5 ">
+      <ChannelRow {...x} />
+    </li>;
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div class="flex flex-col w-full h-screen bg-blue-200">
+      <div class="self-center py-4 align-center">
+        <div class="flex flex-col justify-center">
+          <img
+            alt="Avatar of Alexander Alemayhu"
+            class="self-center w-16 rounded-full"
+            src="/logo.png" />
+        </div>
+        <div class="py-2">
+          <a class="text-white" href="https://twitter.com/aalemayhu">@aalemayhu</a>
+        </div>
+      </div>
+      <div class="mx-2 text-center text-gray-700">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          Below are links to my various YouTube channels, click on the red button to
+          subscribe!
+    </p>
+      </div>
+      <ul class="flex flex-col">
+        {channels}
+      </ul>
     </div>
   );
 }
