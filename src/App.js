474f0e338524aaaf43784d6f360eed50a6a74e4b
Alexander Alemayhu <alexander@alemayhu.com>
2021-07-12 14:35:07 +0200

    sort channels

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ let data = [
     link: 'https://youtube.com/c/alexanderalemayhu'
   },
   {
-    name: 'Alemayhu VODs',
-    thumbnail: '/avatar/portrait_1.jpeg',
-    description: 'Hour long old exports from my Twitch channel 😊',
-    link: 'https://www.youtube.com/c/AlemayhuVODS'
-  },
-  {
     name: 'Anki in Minutes',
     thumbnail: '/avatar/aim.png',
-    description: 'Short and sweet tutorials in Anki 🌟',
+    description: 'Short and sweet tutorials about Anki 🌟',
     link: 'https://www.youtube.com/channel/UCH6xln-f73LO1_1vDCtgQeA'
-  },
-  {
-    name: 'Japanese with Alexander',
-    thumbnail: 'avatar/aa-jp.jpeg',
-    description: 'documenting my journey learning Japanese',
-    link: 'https://www.youtube.com/channel/UCHfcPigjdtXb6RNcn-5CLZA'
   },
   {
     name: 'Alexander Plays Games',
     thumbnail: 'https://yt3.ggpht.com/nWaTIB3YmA0_Js4AK40w1CGpjE_gmytPc3e8NDpQZuKdn1pZQymwKwE1R81d0X_log0lbzc0=s176-c-k-c0x00ffffff-no-rj',
     description: 'My gaming channel 🕹',
     link: 'https://www.youtube.com/channel/UCvduI6Kq1SyuDAOyHabVNnQ'
+  },
+  {
+    name: 'Alemayhu VODs',
+    thumbnail: '/avatar/portrait_1.jpeg',
+    description: 'Hour long old exports from my Twitch channel 😊',
+    link: 'https://www.youtube.com/c/AlemayhuVODS'
+  },
+  {
+    name: 'Japanese with Alexander',
+    thumbnail: 'avatar/aa-jp.jpeg',
+    description: 'documenting my journey learning Japanese',
+    link: 'https://www.youtube.com/channel/UCHfcPigjdtXb6RNcn-5CLZA'
   }
 ];
 
